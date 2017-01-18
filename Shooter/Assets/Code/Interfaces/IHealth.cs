43db7374542d8b62be3d5768205019db869edf91
Henri Riutta <henri.riutta@cs.tamk.fi>
2017-01-18 14:03:42 +0200

    Tokan kerran setit.

---
+++ Shooter/Assets/Code/Interfaces/IHealth.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TAMKShooter
+{
+    public class HealthChangedEventArghs : EventArgs
+    {
+        // The property which contains the amount of health unit has.
+        public int CurrentHealth { get; private set; }
+        /// <summary>
+        /// Constructor which takes the amount of unit's health as a parameter.
+        /// </summary>
+        public HealthChangedEventArghs(int currentHealth)
+        {
+            CurrentHealth = currentHealth;
+        }
+    }
+
+    public delegate void HealthChangedDelegate(object sender, HealthChangedEventArghs args);
+
+    public interface IHealth
+    {
+        int CurrentHealth { get; set; }
+        /// <summary>
+        /// Reduces health when called.
+        /// </summary>
+        /// <param name="damage">Amount of health reduced</param>
+        /// <returns>True, if health reaches zero, false otherwise</returns>         
+        bool TakeDamage(int damage);
+
+        /// <summary>
+        /// This event is fired every time the health changes.
+        /// </summary>
+        event HealthChangedDelegate HealthChanged;
+    }
+}
---
+++ Shooter/Assets/Code/Interfaces/IMover.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace TAMKShooter
+{
+    public interface IMover
+    {
+        // Position of this object in world space
+        Vector3 Position { get; set; }
+        // Rotation of this object in world space
+        Quaternion Rotation { get; set; }
+        // The speed of this mover
+        float Speed { get; }
+
+        // Moves toward a targetPosition
+        void MoveTowardPosition(Vector3 targetPosition);
+        // Move to direction 'direction'
+        void MoveToDirection(Vector3 direction);
+        // Rotate toward position 'targetPosition'
+        void RotateTowardPosition(Vector3 targetPosition);
+
+    }
+}
